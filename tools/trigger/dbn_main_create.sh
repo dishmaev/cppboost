b882f96c5b8e1ba7545a131430e0450ca8224398
dishmaev <idax@rambler.ru>
2017-12-01 12:08:30 +0300

    changes

--- tools/trigger/dbn_main_create.sh
+++ tools/trigger/dbn_main_create.sh
@@ -4,11 +4,11 @@
 
 readonly VAR_PARAMETERS='$1 script name without extenstion, $2 suite'
 
-if [ "$#" != "2" ]; then echo "Call syntax: $(basename "$0") $VAR_PARAMETERS"; exit 1; fi
 if [ -r ${1}.ok ]; then rm ${1}.ok; fi
 exec 1>${1}.log
 exec 2>${1}.err
 exec 3>${1}.tst
+if [ "$#" != "2" ]; then echo "Call syntax: $(basename "$0") $VAR_PARAMETERS"; exit 1; fi
 
 ###function
 
